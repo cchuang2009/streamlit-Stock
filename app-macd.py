579019e270fd960088d769d32200b8348c559f26
Chu-Ching Huang <cchuang2009@gmail.com>
2023-07-05 12:02:08 +0800

    Pyecharts app

--- app-macd.py
+++ app-macd.py
@@ -1,8 +1,10 @@
+# https://www.cnblogs.com/WeiRonbbin/p/15871028.html
+
 import yfinance as yf
 import streamlit as st
 import matplotlib.pyplot as plt
 from math import floor
-from helper import *
+from helperMACD import *
 
 #from streamlit_echarts import st_pyecharts
 
@@ -12,6 +14,7 @@ start_date = datetime(2020, 1, 1)
 
 # Get the current date
 current_date = datetime.now()
+end_date = current_date.strftime('%Y-%m-%d')
 
 # Calculate the difference in days
 days_diff = (current_date - start_date).days
@@ -34,6 +37,9 @@ stocks = {
     '6533.TW': '晶心科',
     '6515.TW': '穎崴',
     '6691.TW': '洋基工程',
+    '2027.TW': '大成鋼',
+    '2634.TW': '漢翔',
+    '3017.TW': '奇鋐',
     '3008.TW': '大立光',
     # Add more stocks and their corresponding symbols here
 }
@@ -45,41 +51,61 @@ st.title("Stock MACD Analysis")
 #symbol = st.text_input("Enter a company", "")
 selected_stock = st.selectbox("Select a stock", list(stocks.keys()), format_func=lambda x: f"{x} - {stocks[x]}")
 time_period = st.selectbox("Select a time period", ['3 months', '6 months', '1 year', 'All'])
-
+company_input = st.text_input("Enter a company", "")
 update_button = st.button("Submit")
 
 
 
-end_date = datetime.now().strftime('%Y-%m-%d')
-if time_period == '1 month':
-    start_date = (datetime.now() - timedelta(days=30)).strftime('%Y-%m-%d')
-    ndays=30
-elif time_period == '3 months':
-    start_date = (datetime.now() - timedelta(days=90)).strftime('%Y-%m-%d')
-    ndays=90
-elif time_period == '6 months':
-    start_date = (datetime.now() - timedelta(days=180)).strftime('%Y-%m-%d')
-    ndays=180
-elif time_period == '1 year':
-    start_date = (datetime.now() - timedelta(days=365)).strftime('%Y-%m-%d')
-    ndays=365
-else:
-    start_date = "2020-01-01"
-    ndays=days_diff
+
+
     
     
     
 if update_button:
-   tick = yf.download(selected_stock, start=start_date, end=current_date)
-
+   if company_input: 
+      tick = yf.download(company_input, start=start_date, end=current_date)
+      tick_name = company_input 
+   else: 
+      tick = yf.download(selected_stock, start=start_date, end=current_date)
+      tick_name=selected_stock
+   # compute ma
+   
+   tick = compute_ma(tick)
+    
    # Calculate MACD
    tick_macd = get_macd(tick['Close'], slow=26, fast=12, smooth=9)
 
    # Implement MACD strategy
    buy_price, sell_price, macd_signal = implement_macd_strategy(tick['Close'], tick_macd)
+    
+    
+   if time_period == '1 month':
+       start_date = (datetime.now() - timedelta(days=30)).strftime('%Y-%m-%d')
+       ndays=30
+   elif time_period == '3 months':
+       start_date = (datetime.now() - timedelta(days=90)).strftime('%Y-%m-%d')
+       ndays=90
+   elif time_period == '6 months':
+        start_date = (datetime.now() - timedelta(days=180)).strftime('%Y-%m-%d')
+        ndays=180
+   elif time_period == '1 year':
+        start_date = (datetime.now() - timedelta(days=365)).strftime('%Y-%m-%d')
+        ndays=365
+   else:
+        start_date = "2020-01-01"
+        ndays=days_diff
+   
+   tick=tick.tail(ndays)
+   tick_macd=tick_macd.tail(ndays)
+   buy_price=buy_price[-ndays:]
+   sell_price=sell_price[-ndays:]
+   macd_signal=macd_signal[-ndays:]
+   #difs,deas,macd = difs_[-ndays:], deas_[-ndays:],macd_[-ndays:] 
 
    # Visualize MACD signals
-   trading_vis_matplotlib(tick, tick_macd, buy_price, sell_price)
+   #trading_vis_matplotlib(tick, tick_macd, buy_price, sell_price)
+    
+   trading_vis_pyecharts(tick, tick_name, tick_macd, buy_price, sell_price) 
 
    # Calculate position and perform backtest
    tick_stradegy=calculate_strategy(macd_signal, tick_macd, tick)
